24ef90252b730f76277e622d0c65f385efde76ff
Gabriel Lins <gabrielinsmelo@gmail.com>
2022-01-08 09:42:08 -0300

    feat: add new folowers monitoring

--- compare.py
+++ compare.py
@@ -20,16 +20,21 @@ with open(FOLLOWING) as f:
 
 lost = [follower for follower in last if follower not in current]
 dont_follow_back = [f for f in following if f not in current]
+new_followers = [f for f in current if f not in last]
 
 if (configs["resultToFile"]):
   with open(RESULTFILE, "w") as f:
-    f.write(f"Lost followers ({len(lost)}):\n\n")
+    f.write(f"- Lost followers ({len(lost)}):\n\n")
     for l in lost:
       f.write(l + "\n")
 
-    f.write(f"\nDont follow you back ({len(dont_follow_back)}):\n\n")
+    f.write(f"\n- Dont follow you back ({len(dont_follow_back)}):\n\n")
     for d in dont_follow_back:
       f.write(d + "\n")
+
+    f.write(f"\n- New folowers ({len(new_followers)}):\n\n")
+    for n in new_followers:
+      f.write(n + "\n")
   print(f"Results written to {RESULTFILE}")
 else:
   print(f"\nLost followers ({len(lost)}):")
@@ -37,3 +42,6 @@ else:
 
   print(f"\nDont follow you back ({len(dont_follow_back)}):")
   print(dont_follow_back)
+
+  print(f"\nNew followers ({len(new_followers)}):")
+  print(new_followers)
